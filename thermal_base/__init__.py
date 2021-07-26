5bcea71f50e60317977fac98cc0265f59e192361
Mukhesh P S <mukheshpugalendhi@gmail.com>
2021-07-27 01:42:06 +0530

    Update readme, Code QA, import structure (#3)
    * v-1.0.0
    
    * updated README
    
    * updated README
    
    * PR changes
    
    * Create qualitycheck.yml
    
    * reformatted
    
    * readme badge
    
    * fixed typo
    
    * fixed minor error
    
    * fix #2
    
    * Remove black check (#1)
    
    Conflicting with pylama
    
    * updated README
    
    * Update usage instructions
    
    Co-authored-by: RS Nikhil Krishna <rsnk96@users.noreply.github.com>

---
+++ thermal_base/__init__.py
@@ -0,0 +1,4 @@
+"""Module imports."""
+from . import utils
+from .thermal_base import (ThermalImage, ThermalImageAnnotation,
+                           ThermalSeqVideo, get_thermal_image_from_file)
